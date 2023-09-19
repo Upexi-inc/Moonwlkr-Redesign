91c45cf39bc3198abdb40056dbd8e8c328d9c4aa
alice-upexi <alice@upexi.com>
2023-09-19 16:27:22 +0100

    Changes to calculate and update pricing functions to include product variations
    Created the calculatePrice function. It multiplies the quantity value by a parameter value.
    
    This parameter will be a regular price or a sale price according to these different scenarios:
    1. Product has no variations and is not on sale - only requires the regular price;
    2. Product has no variations but is on sale - requires both regular and sale prices;
    3. Product has variations and all are on sale - requires both regular and sale prices but they're taken from the variations form JSON objects;
    
    The parameter is passed on the price function that includes the scenarios above.
    
    For the last scenario I created a variable to contain the index of the clicked variation button, as a way to then access that same index on the variation form json objects. The default value is 0 because that's the default variation on page load.
    
    On the variation forEach click event listener, the index variable is updated with the index of the button that was just clicked. Then it runs the price() function and the updatePriceElements() function.
    
    My next step is to test this on moonwlkr dev.

--- dynamic-pricing.js
+++ dynamic-pricing.js
@@ -23,39 +23,110 @@ window.addEventListener('DOMContentLoaded', () => {
         //declare variant buttons to use them later to listen to a click event - each variant has different price values and we have to take that into account
         const variantButtons = document.querySelectorAll('.button-variable-item');
 
+        //grab variation form that contains important info in its json
+        const variantionFormContainer = document.querySelector('form.variations_form');
+
+        //grab product variations from the variation form json
+        //this contains the display_price (price on sale) and display_regular_price (regular price) values required to calculate the price of the product
+        const formVariationsJSON = JSON.parse(variantionFormContainer.dataset.product_variations);
+
+        let clickedBtnIdx = 0;
+
+        //update price when variant is clicked
         variantButtons.forEach(button => {
             button.addEventListener('click', function () {
-                let clickedBtnIdx = Array.from(variantButtons).indexOf(button);
-
-                const variantionFormContainer = document.querySelector('form.variations_form');
-
-                const formVariationsJSON = JSON.parse(variantionFormContainer.dataset.product_variations);
-
-                regularPrice = Number(formVariationsJSON[clickedBtnIdx].display_regular_price);
-
-                salePrice = Number(formVariationsJSON[clickedBtnIdx].display_price);
-
-                let variantRegularPrice = regularPrice * inputQtyNumber;
-                let variantSalePrice = salePrice * inputQtyNumber;
-
-                document.querySelector('.redesign-product-price del .woocommerce-Price-amount bdi').innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${variantRegularPrice}`
-
-                document.querySelector('.redesign-product-price ins .woocommerce-Price-amount bdi').innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${variantSalePrice}`
+                //assign new clicked button index value 
+                clickedBtnIdx = Array.from(variantButtons).indexOf(button);
+                //run the price function to get the new variant prices
+                price();
+                //run the updatePriceElements function to change price values on their respective html elements
+                updatePriceElements();
             });
         });
 
         //FUNCTIONS
 
-        //calculate price functions
+        //calculate price function
+        function calculatePrice(priceValue) {
+            //multiply quantity value by price value
+            return (inputQtyNumber * priceValue).toFixed(2);
+        }
+
+        //function to calculate prices based on different scenarios
+        function price() {
+            let regularPriceValue;
+            let salePriceValue;
+
+            if (document.querySelector('.product-display span.onsale') !== null) {
+                //scenario 1: if the product is on sale pass regular and sale price parameters onto the calculatePrice function
+                regularPriceValue = calculatePrice(regularPrice);
+                salePriceValue = calculatePrice(salePrice);
+
+                if (document.querySelector('form.cart').classList.contains('variations_form')) {
+                    //scenario 2: if the product is both on sale and has variants, pass the clicked variant prices as parameters onto the calculatePrice function
+                    regularPriceValue = calculatePrice(Number(formVariationsJSON[clickedBtnIdx].display_regular_price));
+                    salePriceValue = calculatePrice(Number(formVariationsJSON[clickedBtnIdx].display_price));
+                }
+            } else {
+                //scenario 3: product is not on sale and does not have variants
+                regularPriceValue = calculatePrice(item.price);
+            }
+
+            //return the two prices values as an array to be used in the updatePriceElements function
+            return [regularPriceValue, salePriceValue];
+        }
+
+        function updatePriceElements() {
+            //grab prices array from the price function
+            let prices = price();
+
+            //declare regular price container as an empty variable as the container is different depending on the scenario
+            let regularPriceContainer;
+
+            //grab regular price from the prices array
+            let regularPriceNumber = prices[0];
+
+            //if item is on sale
+            if (document.querySelector('.product-display span.onsale') !== null) {
+                //declare the sale price HTML element
+                let salePriceContainer = document.querySelector('.redesign-product-price ins .woocommerce-Price-amount bdi');
+
+                //assign sale price from the prices array to a variable 
+                let salePriceNumber = prices[1];
+
+                //change sale price inner html to include the calculated sale price
+                salePriceContainer.innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${salePriceNumber}`;
+
+                //assign new element to regularPriceContainer
+                regularPriceContainer = document.querySelector('.redesign-product-price del .woocommerce-Price-amount bdi');
+
+                //change regular price inner html to include the calculated regular price
+                regularPriceContainer.innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${regularPriceNumber}`;
+
+            } else {
+
+                //if product is not on sale
+                //assign regular price container element 
+                regularPriceContainer = document.querySelector('.redesign-product-price .woocommerce-Price-amount bdi');
+
+                //change regular price inner html to include the calculated regular price
+                regularPriceContainer.innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${regularPriceNumber}`;
+            }
+        }
+
+        /*
         //calculate regular price
-        function calculateRegularPrice() {
+        function calculateRegularPrice(priceValue) {
             //declare variable to hold the result of this calculation
-            let regularPriceResult;
+            let regularPriceResult = (inputQtyNumber * priceValue).toFixed(2);
 
             //check to see if item is on sale - this is important as it will determine where we'll get the regular price value
             if (document.querySelector('.product-display span.onsale') !== null) {
                 //store calculation in variable - sale price x quantity
                 //if it's on sale, it will grab the already declared regularPrice variable value
+                if (document.querySelector('form.cart').classList.contains('variations_form')) {
+                    regularPrice = Number(formVariationsJSON[clickedBtnIdx].display_regular_price);
+                }
                 regularPriceResult = inputQtyNumber * regularPrice;
             } else {
                 //if it's not on sale, the price value is grabbed from the item global variable
@@ -69,9 +140,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         //calculate sale price
-        function calculateSalePrice() {
+        function calculateSalePrice(param) {
             //store calculation in variable - sale price x quantity
-            let salePriceResult = inputQtyNumber * salePrice;
+            let salePriceResult = inputQtyNumber * param;
             //return calculation result to be used in other functions
             return salePriceResult;
         }
@@ -81,13 +152,22 @@ window.addEventListener('DOMContentLoaded', () => {
             //declare regularPriceContainer with no value as of yet. this is because if the product is not on sale, there will be no distinction between regular and sale prices HTML elements - so no <del> or <ins> elements
             let regularPriceContainer;
 
-            //update regular price first as product may not be on sale
-            let regularPriceNumber = calculateRegularPrice();
+            let regularPriceNumber = calculateRegularPrice(item.price);
 
             //check to see if item is on sale - this means that the price HTML elements will be different <del> and <ins>
             if (document.querySelector('.product-display span.onsale') !== null) {
+                regularPriceNumber = calculateRegularPrice(regularPrice);
+
                 //we only need to calculate the sale price inside this condition, otherwise it will throw an error in products that are not on sale
-                let salePriceNumber = calculateSalePrice();
+                let salePriceNumber;
+
+                if (document.querySelector('form.cart').classList.contains('variations_form')) {
+                    regularPriceNumber = calculateRegularPrice(Number(formVariationsJSON[clickedBtnIdx].display_regular_price));
+
+                    salePriceNumber = calculateSalePrice(Number(formVariationsJSON[clickedBtnIdx].display_price));
+                } else {
+                    salePriceNumber = calculateSalePrice(salePrice);
+                }
 
                 //grab sale price html element
                 const salePriceContainer = document.querySelector('.redesign-product-price ins .woocommerce-Price-amount bdi');
@@ -105,7 +185,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 //change regular price inner html to reflect new price
                 regularPriceContainer.innerHTML = `<span class="woocommerce-Price-currencySymbol">$</span>${regularPriceNumber}`;
             }
-        }
+        }*/
 
 
     }, "500");
