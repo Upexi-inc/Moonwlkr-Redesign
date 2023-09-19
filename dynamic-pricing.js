35d1697763cf30023f405191c71119fb346345e1
alice-upexi <alice@upexi.com>
2023-09-19 10:31:47 +0100

    Created separate file for dynamic pricing script

---
+++ dynamic-pricing.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //setTimeout to allow all elements to load that for some reason don't load with DOM
+    setTimeout(() => {
+
+    }, "500");
+})
